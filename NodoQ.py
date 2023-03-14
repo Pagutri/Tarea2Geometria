6c6140bc380791996d0d5b34bebe63040a54b6d8
Salma Gutierrez <pagutri@gamil.com>
2023-03-14 17:48:31 -0600

    Pasar NodoQ a otro archivo

---
+++ NodoQ.py
@@ -0,0 +1,38 @@
+class NodoQ:
+    """Arbol binario para guardar los puntos evento. Cada nodo
+    guarda el punto y el segmento que comienza en ese punto, o
+    'None' si ningun segmento comienza en ese punto. El orden
+    va de menor a mayor coordenada 'y' del punto. Puntos con la
+    misma coordenada 'y' van de menor a mayor coordenada 'x'."""
+    def __init__(self, punto, segmento):
+        self.key = [punto, segmento]
+        self.left = None
+        self.right = None
+        
+def insertar(nodo, punto, segmento):
+    # Crear nuevo nodo
+    if nodo is None:
+        return NodoQ(punto, segmento)
+        
+    # Elegir posicion del nodo segun coordenada 'y'
+    if punto.y < nodo.key[0].y:
+        nodo.left = insertar(nodo.left, punto, segmento)
+    elif punto.y > nodo.key[0].y:
+        nodo.right = insertar(nodo.right, punto, segmento)
+    # Elegir posicion del nodo segun coordenada 'x'
+    elif punto.x < nodo.key[0].x:
+        nodo.left = insertar(nodo.left, punto, segmento)
+    elif punto.x > nodo.key[0].x:
+        nodo.right = insertar(nodo.right, punto, segmento)
+        #elif punto.x = nodo.key[0].x:
+        # El punto a insertar no puede tener las mismas coordenadas 
+        # que la de ningun nodo en el arbol porque no queremos
+        # repetir eventos. Escribir funcion 'buscar_en_arbol()'
+        # y correrla antes de insertar() para evitar esto.
+    return nodo
+
+def inorder(root):
+    if root is not None:
+        inorder(root.left)
+        print(root.key[0].x, root.key[0].y, end=" ")
+        inorder(root.right)
