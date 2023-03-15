df7aa22ddf791bb472c52ab059553095da036db2
Salma Gutierrez <pagutri@gamil.com>
2023-03-15 16:28:48 -0600

    Listo el orden de los segmentos

--- Segment.py
+++ Segment.py
@@ -1,34 +1,33 @@
 from Point import Point
+import matplotlib.pyplot as plt
 
 class Segment():
     def __init__(self, p1, p2):
-    	self.p1 = p1
-    	self.p2 = p2
+        self.p1 = p1
+        self.p2 = p2
 
     def __lt__(self, s):
-    	"""Segmentos más a la izquierda son menores.
-    	Esta sobrecarga sólo aplica para cuando recién
-    	se inserta un segmento en el árbol, no para
-    	cuando se hace swap porque los puntos de cruce
-    	son problemáticos."""
+        """Segmentos más a la izquierda son menores.
+        Esta sobrecarga sólo aplica para cuando recién
+        se inserta un segmento en el árbol, no para
+        cuando se hace swap porque los puntos de cruce
+        son problemáticos."""
         if self.p1.x < s.p1.x:
             return True
         elif self.p1.x > s.p1.x:
             return False
         elif self.p1.y == s.p1.y: # Caso mismo punto inicial
-        	return self.p2.x < s.p2.x
+            return self.p2.x < s.p2.x
         elif self.p1.y > s.p1.y:
-        	
-
-
-    def __gt__(self, p):
-        if self.y < p.y:
-            return False
-        elif self.y > p.y:
-            return True
+            return self.p2.x < s.p1.x
         else:
-            if self.x < p.x:
-                return True
-            else:
-                return False
+            return self.p1.x < s.p2.x
+    
+    def dibujar(self, label = None):
+        plt.plot([self.p1.x, self.p2.x], [self.p1.y, self.p2.y], label = label)
+        #return 1
+        
+        
+    # Tal vez pueda usar el > para la comparacion
+    # despues de un punto de cruce
 
