bb8dd0cbe14dbc14e7ce859a6dc33e0f811c2ed7
Salma Gutierrez <pagutri@gamil.com>
2023-03-21 10:08:40 -0600

    Lista la funcion que prueba intersecciones

--- Funciones.py
+++ Funciones.py
@@ -1,4 +1,5 @@
 from Point import Point
+from Segment import Segment
 
 def on_segment(pi, pj, pk):
     if min(pi.x, pj.x) <= pk.x and pk.x <= max(pi.x, pj.x) \
@@ -11,3 +12,25 @@ def direction(pi, pj, pk):
     a = pk - pi
     b = pj - pi
     return a.x*b.y - a.y*b.x
+    
+def segments_intersect(s1, s2):
+    d1 = direction(s2.p1, s2.p2, s1.p1)
+    d2 = direction(s2.p1, s2.p2, s1.p2)
+    d3 = direction(s1.p1, s1.p2, s2.p1)
+    d4 = direction(s1.p1, s1.p2, s2.p2)
+    
+    if ((d1 > 0 and d2 < 0) or (d1 < 0 and d2 > 0)) and \
+       ((d3 > 0 and d4 < 0) or (d3 < 0 and d4 > 0)):
+        return True
+    elif d1 == 0 and on_segment(s2.p1, s2.p2, s1.p1):
+        return True
+    elif d2 == 0 and on_segment(s2.p1, s2.p2, s1.p2):
+        return True
+    elif d3 == 0 and on_segment(s1.p1, s1.p2, s2.p1):
+        return True
+    elif d4 == 0 and on_segment(s1.p1, s1.p2, s2.p2):
+        return True
+    else:
+        return False
+
+
